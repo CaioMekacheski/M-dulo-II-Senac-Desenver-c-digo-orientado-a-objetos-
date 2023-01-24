d2c473691b268069f0902bcfcbbddb2f2ea96052
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-01-24 12:19:31 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -8,6 +8,26 @@ public class Aluno
     String cpf;
     int matricula;
     int idade;
+    float nota1;
+    float nota2;
+    
+    private float media(float valor1, float valor2)
+    {
+        return (valor1 + valor2) / 2;
+    }
+    
+    private float maiorNota()
+    {
+        if(nota1 >= nota2)
+        {
+            return nota1;
+        }
+        
+        else
+        {
+            return nota2;
+        }
+    }
     
     public void descrever()
     {
@@ -16,6 +36,10 @@ public class Aluno
         System.out.println("CPF: \t" + cpf);
         System.out.println("MATRICULA: \t" + matricula);
         System.out.println("IDADE: \t" + idade);
+        System.out.println("NOTA ATIVIDADE 1: \t" + nota1);
+        System.out.println("NOTA ATIVIDADE 2: \t" + nota2);
+        System.out.println("\nMÉDIA: " + media(nota1, nota2));
+        System.out.println("MAIOR NOTA: " + maiorNota());
 
     }
 }
