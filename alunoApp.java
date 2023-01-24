c8a13efc49d9ff964270f090dcf39fdd0a1dedcb
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-01-24 12:18:57 -0300

    Update alunoApp.java

--- alunoApp.java
+++ alunoApp.java
@@ -14,16 +14,23 @@ public class Exercicio_java
         aluno01.cpf = "24848718941";
         aluno01.matricula = 150;
         aluno01.idade = 36;
+        aluno01.nota1 = 6;
+        aluno01.nota2 = 8;
+                
         
         aluno02.nome = "Letícia Arias";
         aluno02.cpf = "54398115945";
         aluno02.matricula = 151;
         aluno02.idade = 27;
+        aluno02.nota1 = 10;
+        aluno02.nota2 = 9;
         
         aluno03.nome = "Isadora Araújo";
         aluno03.cpf = "2148416921";
         aluno03.matricula = 152;
         aluno03.idade = 10;
+        aluno03.nota1 = 10;
+        aluno03.nota2 = 10;
         
         System.out.println("\t\t ALUNOS MATRICULADOS: \n\n");
         
