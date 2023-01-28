600158901bfd56bc1f755bc159bc3ddd4a6560d0
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-01-28 10:47:10 -0300

    Create atividade1_2_classe_Hospede.java

---
+++ atividade1_2_classe_Hospede.java
@@ -0,0 +1,19 @@
+
+package atividade1_2;
+
+public class Hospede 
+{
+    String nome;
+    int idade;
+   
+    
+    public String nomeHospede()
+    {
+        return nome;
+    }
+    
+    public int idadeHospede()
+    {
+        return idade;
+    }
+}
