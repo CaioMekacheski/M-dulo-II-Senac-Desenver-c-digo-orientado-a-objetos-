bdd038e727453018cff0ae761a83dd3e0eaec2e7
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-01-31 10:53:52 -0300

    Update atividade1_1_classe_Quarto.java

--- atividade1_1_classe_Quarto.java
+++ atividade1_1_classe_Quarto.java
@@ -1,11 +1,31 @@
-
+//Atualizado 31/01/2023 10:53
 package atividade1_2;
 
 public class Quarto 
 {
-    char id;
-    String disp;
-    Hospede hospede;
+    private char id;
+    private String disp;
+    private Hospede hospede;
+        
+    public void setId(char id)
+    {
+        this.id = id;
+    }
+    
+    public void setHospede(Hospede hospede)
+    {
+        this.hospede = hospede;
+    }
+    
+    public char getId()
+    {
+        return id;
+    }
+    
+    public Hospede getHospede()
+    {
+        return hospede;
+    }
     
     public String checaDisp()
     {
