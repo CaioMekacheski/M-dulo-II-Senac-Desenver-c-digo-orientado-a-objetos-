9d56a9e0f41822f6fb7cc51450411080725917ac
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-02-04 21:11:24 -0300

    Create atividade2_classe_Venda.java

---
+++ atividade2_classe_Venda.java
@@ -0,0 +1,64 @@
+//Criado 04/02/2023 21:05
+package atividade2;
+
+
+public class Venda 
+{
+    //Atributos
+    private String cliente;
+    private String pagamento;
+    private PacoteDeViagem pacote;
+    
+    //Construtor
+    public Venda(String cliente, String pagamento, PacoteDeViagem pacote)
+    {
+        this.cliente = cliente;
+        this.pagamento = pagamento;
+        this.pacote = pacote;
+    }
+    
+    //Setters
+    public void setCliente(String cliente)
+    {
+        this.cliente = cliente;
+    }
+    
+    public void setPagamento(String pagamento)
+    {
+        this.pagamento = pagamento;
+    }
+    
+    public void setPacote(PacoteDeViagem pacote)
+    {
+        this.pacote = pacote;
+    }
+    
+    //Getters
+    public String getCliente()
+    {
+        return cliente;
+    }
+    
+    public String getPagamento()
+    {
+        return pagamento;
+    }
+    
+    public PacoteDeViagem getPacote()
+    {
+        return pacote;
+    }
+    
+    //Métodos
+    public float convertRealUSD(float real, float usd)
+    {
+        return real / usd;
+    }
+    
+    public String exibeTotais(float real, float usd)
+    {
+        return "Total em reais: R$ " + real + "\n Total em Dólares: US$ " + usd;
+    }
+            
+    
+}
