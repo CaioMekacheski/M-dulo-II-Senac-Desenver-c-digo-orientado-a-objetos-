9c042b098998a3094ddd81d9d389a076f99fc3f4
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-02-26 16:20:23 -0300

    Update atividade4_classe_Pagamentos.java

--- atividade4_classe_Pagamentos.java
+++ atividade4_classe_Pagamentos.java
@@ -1,14 +1,18 @@
-//Criado 25/02/2023 19:01
+//Criado 26/02/2023 16:19
 
 package atividade4;
 
+import static atividade4.Atividade4.escreva;
+import static atividade4.Atividade4.isFloat;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Pagamentos
 {
     //Atributos
-    public String nomeEmpresa;
-    public ArrayList<Imposto> listImpostos;
+    public Scanner entrada = new Scanner(System.in);
+    private String nomeEmpresa;
+    private ArrayList<Imposto> listImpostos;
     
     //Construtor
     public Pagamentos(String nomeEmpresa, ArrayList<Imposto> impostos)
@@ -17,9 +21,69 @@ public class Pagamentos
         this.listImpostos = impostos;
     }
     
-    //Getters e Setters
+    //Getters and Setters
     public String getNomeEmpresa()
     {
         return this.nomeEmpresa;
+    }
+    
+    public ArrayList<Imposto> getlistImpostos()
+    {
+        return this.listImpostos;
+    }
+    
+    public void setNomeEmpresa(String nomeEmpresa)
+    {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+    
+    public void setListImpostos(ArrayList<Imposto> impostos)
+    {
+        this.listImpostos = impostos;
+    }
+    
+    //Metodos
+    public float entradaValor(String descr)
+    {
+        float valor;
+        char chValor;
+        String strValor;
+        String caracterValor = "";
+        do
+        {
+            //Entrada
+            escreva(descr, 2);
+            strValor = entrada.nextLine();
+            
+            //Verifica se o valor digitado é float
+            for(int i = 0; i < strValor.length(); i++)
+            {
+                chValor = strValor.charAt(i);
+                caracterValor = Character.toString(chValor);
+
+                if(isFloat(caracterValor) != true)
+                {
+                    escreva("Valor inválido. Digite um valor numérico.", 1);
+                    break;
+                }
+            }
+        }
+        while(isFloat(caracterValor) != true);
+        
+        //Converte o valor digitado em float
+        valor = Float.parseFloat(strValor);
+        
+        return valor;
+    }
+    
+    public void listPagamentos()
+    {
+        escreva("\nLISTA DE PAGAMENTOS", 1);
+        escreva("Empresa: " + nomeEmpresa, 1);
+        
+        for(int i = 0; i < listImpostos.size(); i++)
+        {
+            escreva(listImpostos.get(i).exibeDados(), 1);
+        }
     }
 }
